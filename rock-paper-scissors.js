429632c42c0c87d66637f92d081372cd081b184c
alprodi <106893267+alprodi@users.noreply.github.com>
2022-07-15 19:01:33 -0400

    Comment out a plan/whiteboard

---
+++ rock-paper-scissors.js
@@ -0,0 +1,14 @@
+//Create function getComputerChoice which will randomly return 'Rock', 'Paper', or 'Scissors.'
+
+//Put 'Rock', 'Paper', and 'Scissors' into an array
+//Get a random number (between 0 and 1? If so, mult by 3).
+
+//Temp Test: Print the result
+
+//Create function that takes the playerSelection parameter (case-insensitive) and computerSelection parameter, 
+
+//Return result via a string that declares the winner.
+
+
+//After the above works:
+//Write a new function called game(), calling the playRound function inside to play a 5 round game that keeps score and reports a winner or loser at the end.
