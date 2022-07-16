1b4bfa159fac386e9deabada9afa886a8d49ad16
alprodi <106893267+alprodi@users.noreply.github.com>
2022-07-15 20:31:24 -0400

    Keep track of score

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,8 @@
 //Declare the 3 possible choices as a constant
 const choices = ["ROCK", "PAPER", "SCISSORS"]
-
+//Set default scores to 0.
+let computerScore = 0;
+let playerScore = 0;
 //Create function getComputerChoice which will randomly return 'Rock', 'Paper', or 'Scissors.'
 //Put 'Rock', 'Paper', and 'Scissors' into an array
 //Get a random number (between 0 and 1? If so, mult by 3).
@@ -8,7 +10,7 @@ function getComputerChoice(){
     return(choices[Math.floor(Math.random() * 3)]);
 }
 //Create function that takes the playerSelection parameter (case-insensitive) and computerSelection parameter, 
-function getPlayerChoice(){
+function playRound(){
     let playerSelection = prompt("Rock, Paper, or Scissors?").toUpperCase();
     let computerSelection = getComputerChoice();
     //Return result via a string that declares the winner.
@@ -25,6 +27,7 @@ function getPlayerChoice(){
             {
             console.log("You picked " + playerSelection + ", and the computer picked " + computerSelection + ".");
             console.log("You won! How rare.");
+            console.log(playerScore++);
             }
     //Check if computer wins
     else if( 
@@ -34,6 +37,7 @@ function getPlayerChoice(){
             {
             console.log("You picked " + playerSelection + ", and the computer picked " + computerSelection + ".");
             console.log("You lost!");
+            console.log(computerScore++);
             }
     //Check if the player didn't put in a valid choice
     else if(
@@ -41,12 +45,35 @@ function getPlayerChoice(){
         playerSelection != "SCISSORS" &&
         playerSelection != "PAPER")
             {
-             console.log("You picked " + playerSelection + ", and the computer picked " + computerSelection + ".");
+            console.log("You picked " + playerSelection + ", and the computer picked " + computerSelection + ".");
             console.log("You didn't input either Rock, Paper, or Scissors. Try again");
             }
     //Catch all other weird errors. This shouldn't be possible?!
     else{console.log("Something went terribly wrong to get this message.")};
+    console.log ("The current score is PLAYER: " + playerScore + ". COMPUTER: " + computerScore + ".");
 }
+
+//Play the game
+function game(){
+    //Reset scores
+    playerScore = 0;
+    computerScore = 0;
+    while ((playerScore + computerScore) <5) {
+        playRound();
+    }
+//Calculate final score
+    console.log("The final score is Player: " + playerScore + ". Computer: " + computerScore + ".");
+    if (playerScore > computerScore){
+        console.log("You won!");}
+    else if (playerScore < computerScore){
+        console.log("You lost!");}
+    else if (playerScore == computerScore){
+        console.log("It's a tie!");}
+    else {console.log("This is an error message. Something went wrong, what did you do?")}
+   
+}
+    
+game();
 
 
 //After the above works:
